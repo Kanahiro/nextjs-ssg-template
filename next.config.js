d8b439688a39871f647c601e033b475e0ba0c749
Kanahiro <mediterranean1769@gmail.com>
2021-07-17 17:41:27 +0900

    ssg-template

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const isProd = process.env.NODE_ENV == 'production';
+const basePath = isProd ? '/nextjs-trial' : '';
+
 module.exports = {
-    assetPrefix: '/nextjs-trial',
+    assetPrefix: basePath,
+    trailingSlash: true,
+    publicRuntimeConfig: { basePath },
 };
